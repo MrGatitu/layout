a460d81efb0406ac335b50e4eab3ab49b163eace
MrGatitu <mrgatituh@gmail.com>
2024-06-21 22:16:16 +0300

    Layout

---
+++ lib/testlayout.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Testlayout extends StatelessWidget {
+  const Testlayout({
+    super.key,
+    required this.name,
+    required this.location,
+  });
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ,
+      ),
+    );
+  }
+}
